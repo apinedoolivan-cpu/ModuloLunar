651d34de500ea73cd9f22c0d4151b55f2e5c5905
apinedo-cpu <apinedoolivan@gmail.com>
2025-12-12 10:03:09 +0100

    Cambio a la ruta y arreglo formulario astronauta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
-import { FormularioAstronautaComponent } from './components/form-astronauta/form-astronauta';
 import { SeleccionCriterioComponent } from './components/seleccion-criterio/seleccion-criterio';
 
 export const routes: Routes = [
-  { path: 'form-astronauta', component: FormularioAstronautaComponent },
-  { path: 'servicio-criterio', component: SeleccionCriterioComponent },
-  { path: '', redirectTo: 'servicio-criterio', pathMatch: 'full' },
+  { path: '', component: SeleccionCriterioComponent }, 
 ];
 
 export const AppRouting = RouterModule.forRoot(routes, {
--- src/app/services/form-astronauta.ts
+++ src/app/services/form-astronauta.ts
@@ -15,7 +15,6 @@ export class AstronautaValidationService {
       const edad = form.get('edad');
 
       if (id?.invalid) errores.push('El ID debe tener el formato AGM001 (3 letras + 3 números)');
-      if (nombre?.invalid || !nombre?.value?.trim()) errores.push('El nombre es obligatorio');
       if (edad?.invalid || edad?.value < 18 || edad?.value > 65) errores.push('La edad debe estar entre 18 y 65');
 
       return errores.length > 0 ? "ERROR: "+errores.join(', ') : null;
